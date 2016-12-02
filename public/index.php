e612a4cae3ef3cbf029d1668237013284c6c4d6c
JapSeyz <Jesper@oestertoft.dk>
2016-12-03 00:11:03 +0100

    Init

---
+++ public/index.php
@@ -0,0 +1,79 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Arduino - JapSeyz</title>
+
+    <!-- Styles -->
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css"
+          integrity="sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u" crossorigin="anonymous">
+    <link href="/css/app.css" rel="stylesheet">
+</head>
+<body>
+<div id="app">
+    <nav class="navbar navbar-default navbar-static-top">
+        <div class="container">
+            <div class="navbar-header">
+
+                <!-- Collapsed Hamburger -->
+                <button type="button" class="navbar-toggle collapsed" data-toggle="collapse"
+                        data-target="#app-navbar-collapse">
+                    <span class="sr-only">Toggle Navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <!-- Branding Image -->
+                <a class="navbar-brand logo-text" href="/">
+                    JapSeyz
+                </a>
+            </div>
+
+            <div class="collapse navbar-collapse" id="app-navbar-collapse">
+                <!-- Left Side Of Navbar -->
+            </div>
+        </div>
+    </nav>
+
+    <div class="container-fluid text-center">
+
+            <?php
+                // Set the amount of columns to generate
+                $columns = (array_key_exists('columns', $_GET) ? $_GET['columns'] : 25);
+
+                // Set the amount of LEDs in the array
+                $leds = (array_key_exists('leds', $_GET) ? $_GET['leds'] : 8);
+
+                // Generate a fluid layout with x leds in every column
+                    for($i = $columns; $i > 0; $i--){
+                        echo "<div class='column'>";
+                            for($j = $leds; $j > 0; $j--) {
+                                echo "<div class='led'></div>";
+                            }
+                        echo "</div>";
+                    }
+            ?>
+            <br />
+            <button class="btn btn-primary generate">Generate</button>
+            <button class="btn btn-primary load">Load</button>
+
+            <div class="row">
+                <div class="half">
+                    <pre class="output"></pre>
+                </div>
+
+                <div class="half">
+                    <textarea class="load-area"></textarea>
+                </div>
+            </div>
+    </div>
+</div>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-cookie/1.4.1/jquery.cookie.min.js"></script>
+<script src="/js/app.js"></script>
+</body>
+</html>
---
+++ public/js/app.js
@@ -0,0 +1,162 @@
+$(document).ready(function(){
+
+    // If a cookie has been set with data
+    // Load the data and delete the Cookie
+    if($.cookie('data')){
+        $('.load-area').val($.cookie('data'));
+        $.removeCookie('data'); // => true
+
+        setTimeout(function(){
+            $('.load').trigger('click');
+        }, 500);
+    }
+
+    // While holding down the mouse and hovering over an
+    // LED block, change it to the next color in the rotation
+    $(document).mousedown(function() {
+        $(".led").bind('mouseover',function(){
+          changeLED($(this));
+      });
+    })
+    .mouseup(function() {
+        $(".led").unbind('mouseover');
+    });
+    $('.led').mousedown(function() {
+     changeLED($(this));
+ });
+
+    $('.generate').on('click', generate);
+    $('.load').on('click', load);
+});
+
+function changeLED(that){
+    switch(that.attr('data-color')){
+        case 'red':
+        that.attr('data-color', 'green');
+        break;
+
+        case 'green':
+        that.attr('data-color', 'blue');
+        break;
+
+        case 'blue':
+        that.attr('data-color', 'black');
+        break;
+
+        default:
+        that.attr('data-color', 'red');
+        break;
+    }
+};
+
+
+// Mutate the Image into an C-style Byte-string
+function generate(){
+
+    // The Canvas Columns
+    var columns = $('.column');
+
+    // Data holder
+    var data = [];
+
+    // Initialize Variables
+    var leds, cloumn;
+
+    // Loop over all the Columns
+    $.each(columns, function(index, element){
+        // Start the Byte
+        column = '{';
+        leds = $(element).find('.led');
+
+        // Loop over the LEDs
+        $.each(leds, function(key, led){
+            led = $(led);
+
+            switch(led.attr('data-color')){
+                case 'red':
+                    column += "{1,0,0}";
+                    break;
+                case 'green':
+                    column += "{0,1,0}";
+                    break;
+                case 'blue':
+                    column += "{0,0,1}";
+                    break;
+                default:
+                    column += "{0,0,0}";
+                    break;
+            }
+
+                // Add a comma seperator
+                if(typeof leds[key+1] !== 'undefined'){
+                    column += ',';
+                }
+            });
+
+        column += '},\n';
+        data[index] = column;
+    });
+
+    $('.output').html(data);
+}
+
+function load(){
+    // The Canvas Columns
+    var columns = $('.column');
+
+    // Data to Parse
+    var data = $('.load-area').val().replace(/\r?\n|\r/g, '');
+
+    // Initialize Variables
+    var cloumn, leds, domLed;
+    var hasCheckedSize = false;
+
+    // Split the data into Columns
+    data = data.split(/,{{/g);
+
+    // Loop over the Columns in the DOM
+    $.each(columns, function(index, element){
+
+        // Split the LEDs into it's own array
+        column = data[index];
+        leds = column.replace(/{{|}},?/g, '').split(/},{/g);
+        
+        // Loop over all the LEDs
+        $.each(leds, function(key, led){
+
+            // Make sure the size of the canvas matches
+            // that of the Data-to-parse
+            if(!hasCheckedSize){
+                hasCheckedSize = true;
+
+                // Validate the size of the 2x2 arrays
+                if(columns.length !== data.length || leds.length != $($(element).find('.led')).length) {
+                    // Save the Data in a Cookie before the reload
+                    $.cookie('data', $('.load-area').val());
+
+                    // Reload the page with the proper parameters
+                    window.location = '/?columns='+data.length+'&leds='+leds.length
+                }
+            }
+
+            // Get the DOM Led in Question
+            domLed = $($(element).find('.led:nth-child('+(key+1)+')'));
+
+            // Give the LEd the correct color
+            switch(led){
+                case '1,0,0':
+                    domLed.attr('data-color', 'red');
+                    break;
+                case '0,1,0':
+                    domLed.attr('data-color', 'green');
+                    break;
+                case '0,0,1':
+                    domLed.attr('data-color', 'blue');
+                    break;
+                default:
+                    domLed.attr('data-color', 'black');
+                    break;
+            }
+        });
+    });
+}
